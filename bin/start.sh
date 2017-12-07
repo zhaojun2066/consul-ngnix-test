0fb475c836f16fd8379a89452accce5c4be800a8
zhaojun2066 <zhaojun2066@gmail.com>
2017-12-07 21:32:09 +0800

    add web-server more

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+cd `dirname $0`
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+
+nohup  $DEPLOY_DIR/consul-server/consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -bind 0.0.0.0 -client 0.0.0.0 -ui > consul.log 2>&1 &
+nohup  $DEPLOY_DIR/consul-template/consul-template -consul-addr 127.0.0.1:8500 -template $DEPLOY_DIR/consul-template/demo.tomcat.ctempl:$DEPLOY_DIR/nginx/demo.tomcat:"$DEPLOY_DIR/nginx/start.sh" > st_temlate.log 2>&1 &
+
+
+ps -ef|grep nginx | grep -v grep
+if [ $? -ne 0 ]
+then
+  sudo /usr/local/nginx/sbin/nginx -c $DEPLOY_DIR/nginx.conf
+  echo "nginx start"
+else
+  sudo /usr/local/ningx/sbin/nginx -c $DEPLOY_DIR/nginx.conf -s reload
+  echo "nginx reload"
+fi
--- web-server/src/main/java/com/ajun/consul/web/ConsulWeb.java
+++ web-server/src/main/java/com/ajun/consul/web/ConsulWeb.java
@@ -1,7 +1,20 @@
 package com.ajun.consul.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by jufeng on 17-12-7.
  */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class ConsulWeb {
+
+    public static void main(String [] args){
+        SpringApplication.run(ConsulWeb.class, args);
+    }
+
 }
